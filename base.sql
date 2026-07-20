f78217bc8a9aebc84f581c0c9a02d14cde5c2058
RATANDRA Thierry Arsenoh <103485412+ArsenohThierry@users.noreply.github.com>
2026-07-20 17:19:42 +0300

    #

--- base.sql
+++ base.sql
@@ -173,11 +173,6 @@ VALUES
     (3, 500001, 1000000, 2510),
     (3, 1000001, 2000000, 3010);
 
-insert into prefixe (prefixe, id_operateur) values
-('033', 1),
-('034', 2),
-('037', 3);
-
 INSERT INTO client (nom, numero)
 VALUES
 ('Mika', '0371111111'),
